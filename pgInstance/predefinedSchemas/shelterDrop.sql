55eecb5a26686f04658bc900fe2784e44aff2ae8
afig <afig23@gmail.com>
2017-06-07 22:15:34 -0400

    Update shelterDrop to match new shelterCreate
    Since the tables are no longer encapsulated in a specified schema, DROP
    SCHEMA could not longer be used. Tables and views are now dropped
    individually.

--- pgInstance/predefinedSchemas/shelterDrop.sql
+++ pgInstance/predefinedSchemas/shelterDrop.sql
@@ -2,12 +2,23 @@
 --
 --shelterDrop.sql
 --
---Schemas for CS205; Created: 2017-05-31; Modified 2017-05-31
+--Schemas for CS205; Created: 2017-05-31; Modified 2017-06-07
 
 --This script must be run under the role that is the owner of the shelter schema.
 
---This script drops the shelter schema, with the CASCADE option. This option drops all objects
--- in the schema (e.g. tables, view, functions, etc.), also with the CASCADE option. This means
--- that all objects dependent on objects in the schema will also be dropped.
+--This script drops the tables and views from the shelter scenario. Not all tables or views may
+-- exist, depending on the queries the user has run.
 
-DROP SCHEMA shelter CASCADE;
+DROP VIEW IF EXISTS dog_treatment;
+DROP VIEW IF EXISTS treatment_view;
+DROP VIEW IF EXISTS nvl_example;
+DROP TABLE IF EXISTS phone_list;
+DROP TABLE IF EXISTS adoption;
+DROP TABLE IF EXISTS return;
+DROP TABLE IF EXISTS treatment;
+DROP TABLE IF EXISTS assignment;
+DROP TABLE IF EXISTS volunteer;
+DROP TABLE IF EXISTS adopter;
+DROP TABLE IF EXISTS dog;
+DROP TABLE IF EXISTS vet;
+DROP TABLE IF EXISTS responsibility;
