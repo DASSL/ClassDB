bb2088c6b46d8ee774f3a6ad97fae30810ad3c2a
afig <afig23@gmail.com>
2017-06-07 22:03:23 -0400

    Remove schema qualification
    Permissions and the creation of the shelter schema have been removed.
    Also, tables are no longer schema qualified. This is to allow the
    script to be run by individual users who need the set of tables, rather
    than having to copy it from a different schema.

--- pgInstance/predefinedSchemas/shelterCreate.sql
+++ pgInstance/predefinedSchemas/shelterCreate.sql
@@ -2,21 +2,9 @@
 --
 --shelterCreate.sql
 --
---Schemas for CS205; Created: 2017-05-31; Modified 2017-06-01
+--Schemas for CS205; Created: 2017-05-31; Modified 2017-06-07
 
 --The following script should be run as the role desired to be the owner of the shelter schema.
-
---This script creates a schema for the Shelter senario, along with creating the senario's
--- tables and poplating them.
-
-CREATE SCHEMA shelter;
-GRANT USAGE ON SCHEMA shelter TO instructor;
-GRANT USAGE ON SCHEMA shelter TO student;
-GRANT USAGE ON SCHEMA shelter TO admin;
-ALTER DEFAULT PRIVILEGES IN SCHEMA shelter GRANT SELECT ON TABLES TO instructor;
-ALTER DEFAULT PRIVILEGES IN SCHEMA shelter GRANT SELECT ON TABLES TO student;
-ALTER DEFAULT PRIVILEGES IN SCHEMA shelter GRANT SELECT ON TABLES TO admin;
-
 
 --This shelter schema was originally created by Dr. Gancho Ganchev and Julie Gordon as a part
 -- of a workbook to introduce SQL*Plus. This schema was originally implemented for Oracle as
@@ -26,7 +14,7 @@ ALTER DEFAULT PRIVILEGES IN SCHEMA shelter GRANT SELECT ON TABLES TO admin;
 --This schema has been ported to pgSQL for implementation in Postgres 9.6 while making the
 -- fewest changes possible.
 
-CREATE TABLE shelter.dog
+CREATE TABLE dog
 (
   dog_id        CHAR(3) NOT NULL,
   name          VARCHAR(15),
@@ -37,7 +25,7 @@ CREATE TABLE shelter.dog
   PRIMARY KEY(dog_id)
 );
 
-CREATE TABLE shelter.adopter
+CREATE TABLE adopter
 (
   adopter_id CHAR(3),
   fname      VARCHAR(10),
@@ -50,7 +38,7 @@ CREATE TABLE shelter.adopter
   PRIMARY KEY(adopter_id)
 );
 
-CREATE TABLE shelter.volunteer
+CREATE TABLE volunteer
 (
   vol_id CHAR(3),
   fname  VARCHAR(10),
@@ -60,13 +48,13 @@ CREATE TABLE shelter.volunteer
   PRIMARY KEY(vol_id)
 );
 
-CREATE TABLE shelter.responsibility
+CREATE TABLE responsibility
 (
   title VARCHAR(20),
   PRIMARY KEY(title)
 );
 
-CREATE TABLE shelter.vet
+CREATE TABLE vet
 (
   vet_id  CHAR(1),
   fname   VARCHAR(10),
@@ -79,20 +67,20 @@ CREATE TABLE shelter.vet
   PRIMARY KEY(vet_id)
 );
 
-CREATE TABLE shelter.adoption
+CREATE TABLE adoption
 (
   dog_id        CHAR(3),
   adopter_id    CHAR(3),
   vol_id        CHAR(3),
   adoption_date DATE,
   adoption_fee  NUMERIC(5, 2),
-  FOREIGN KEY(dog_id) REFERENCES shelter.dog(dog_id),
-  FOREIGN KEY(adopter_id) REFERENCES shelter.adopter(adopter_id),
-  FOREIGN KEY(vol_id) REFERENCES shelter.volunteer(vol_id),
+  FOREIGN KEY(dog_id) REFERENCES dog(dog_id),
+  FOREIGN KEY(adopter_id) REFERENCES adopter(adopter_id),
+  FOREIGN KEY(vol_id) REFERENCES volunteer(vol_id),
   PRIMARY KEY(dog_id, adopter_id, vol_id)
 );
 
-CREATE TABLE shelter.treatment
+CREATE TABLE treatment
 (
   treatment_id   CHAR(3),
   vet_id         CHAR(1),
@@ -101,27 +89,27 @@ CREATE TABLE shelter.treatment
   description    VARCHAR(20),
   fee            NUMERIC(5, 2),
   discount_rate  NUMERIC(4, 2),
-  FOREIGN KEY(vet_id) REFERENCES shelter.vet(vet_id),
-  FOREIGN KEY(dog_id) REFERENCES shelter.dog(dog_id),
+  FOREIGN KEY(vet_id) REFERENCES vet(vet_id),
+  FOREIGN KEY(dog_id) REFERENCES dog(dog_id),
   PRIMARY KEY(treatment_id)
 );
 
-CREATE TABLE shelter.return
+CREATE TABLE return
 (
   dog_id      CHAR(3),
   adopter_id  CHAR(3),
   return_date DATE,
   reason      VARCHAR(30),
-  FOREIGN KEY(dog_id) REFERENCES shelter.dog(dog_id),
-  FOREIGN KEY(adopter_id) REFERENCES shelter.adopter(adopter_id),
+  FOREIGN KEY(dog_id) REFERENCES dog(dog_id),
+  FOREIGN KEY(adopter_id) REFERENCES adopter(adopter_id),
   PRIMARY KEY(dog_id, adopter_id)
 );
 
-CREATE TABLE shelter.assignment
+CREATE TABLE assignment
 (
   vol_id         CHAR(3),
   responsibility VARCHAR(20),
-  FOREIGN KEY(vol_id) REFERENCES shelter.volunteer(vol_id),
-  FOREIGN KEY(responsibility) REFERENCES shelter.responsibility(title),
+  FOREIGN KEY(vol_id) REFERENCES volunteer(vol_id),
+  FOREIGN KEY(responsibility) REFERENCES responsibility(title),
   PRIMARY KEY(vol_id, responsibility)
 );
