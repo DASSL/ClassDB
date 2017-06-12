14b3db331f668e9765c06a372417ee516bd2a807
Steven Rollo <srrollo@gmail.com>
2017-06-12 18:22:13 -0400

    Fixed a couple format inconsistencies

--- pgInstance/metaFunctions.sql
+++ pgInstance/metaFunctions.sql
@@ -6,24 +6,24 @@
 --Functions for simple access to table metadata; Created: 2017-06-07; Modified 2017-06-07
 
 --Drop old functions and return types if they exist
-DROP FUNCTION IF EXISTS public.list_tables();
-DROP FUNCTION IF EXISTS public.list_tables(TEXT);
+DROP FUNCTION IF EXISTS public.listTables();
+DROP FUNCTION IF EXISTS public.listTables(TEXT);
 DROP FUNCTION IF EXISTS public.describe(TEXT);
 DROP FUNCTION IF EXISTS public.describe(TEXT, TEXT);
 
-DROP TYPE IF EXISTS public.list_tables_return;
-DROP TYPE IF EXISTS public.describe_retrun;
+DROP TYPE IF EXISTS public.listTablesReturn;
+DROP TYPE IF EXISTS public.describeRetrun;
 
 --Prototype of a row that will be returned by the public.list_tables function
 --Matches the output of the contained SELECT query
-CREATE TYPE public.list_tables_return AS (
+CREATE TYPE public.listTablesReturn AS (
    "Name" INFORMATION_SCHEMA.SQL_IDENTIFIER,
    "Type" INFORMATION_SCHEMA.CHARACTER_DATA
 );
 
 --Prototype of a row that will be returned by the public.describe function
 --Matches the output of the contained SELECT query
-CREATE TYPE public.describe_retrun AS (
+CREATE TYPE public.describeRetrun AS (
    "Table Name" INFORMATION_SCHEMA.SQL_IDENTIFIER,
    "Column Name" INFORMATION_SCHEMA.SQL_IDENTIFIER,
    "Data Type" INFORMATION_SCHEMA.CHARACTER_DATA,
@@ -32,8 +32,8 @@ CREATE TYPE public.describe_retrun AS (
 
 --Returns a list of tables and views in the current user's schema
 --Assumes a schema named <username>
-CREATE OR REPLACE FUNCTION public.list_tables()
-RETURNS SETOF public.list_tables_return
+CREATE OR REPLACE FUNCTION public.listTables()
+RETURNS SETOF public.listTablesReturn
 AS $$
    SELECT table_name, table_type
    FROM INFORMATION_SCHEMA.TABLES
@@ -42,8 +42,8 @@ $$
 LANGUAGE SQL;
 
 --Returns a list of tables and views in the specified schema
-CREATE OR REPLACE FUNCTION public.list_tables(TEXT)
-RETURNS SETOF public.list_tables_return
+CREATE OR REPLACE FUNCTION public.listTables(TEXT)
+RETURNS SETOF public.listTablesReturn
 AS $$
    SELECT table_name, table_type
    FROM INFORMATION_SCHEMA.TABLES
@@ -54,7 +54,7 @@ LANGUAGE SQL;
 --Returns a list of columns in the specified table or view
 --Will only work on tables in the user's search_path
 CREATE OR REPLACE FUNCTION public.describe(TEXT)
-RETURNS SETOF public.describe_retrun
+RETURNS SETOF public.describeRetrun
 AS $$
    SELECT table_name, column_name, data_type, character_maximum_length 
    FROM INFORMATION_SCHEMA.COLUMNS 
@@ -65,7 +65,7 @@ LANGUAGE SQL;
 
 --Returns a list of columns in the specified table or view in the specified schema
 CREATE OR REPLACE FUNCTION public.describe(TEXT, TEXT)
-RETURNS SETOF public.describe_retrun
+RETURNS SETOF public.describeRetrun
 AS $$
    SELECT table_name, column_name, data_type, character_maximum_length 
    FROM INFORMATION_SCHEMA.COLUMNS 
