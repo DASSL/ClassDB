18f76a929df330b442e99d0f5fac6567857b5937
Steven Rollo <srrollo@gmail.com>
2017-06-12 16:35:07 -0400

    Added DASSL branding to metaFunctions.sql Changed return types to information_schema types

--- pgInstance/metaFunctions.sql
+++ pgInstance/metaFunctions.sql
@@ -1,9 +1,9 @@
---Steven Rollo
+--Created at the WCSU DASSL
+--By Andrew Figueroa, Steven Rollo and Sean Murthy
 --
 --metaFunctions.sql
 --
 --Functions for simple access to table metadata; Created: 2017-06-07; Modified 2017-06-07
-
 
 --Drop old functions and return types if they exist
 DROP FUNCTION IF EXISTS public.list_tables();
@@ -17,17 +17,17 @@ DROP TYPE IF EXISTS public.describe_retrun;
 --Prototype of a row that will be returned by the public.list_tables function
 --Matches the output of the contained SELECT query
 CREATE TYPE public.list_tables_return AS (
-	"Name" TEXT,
-	"Type" TEXT
+   "Name" INFORMATION_SCHEMA.SQL_IDENTIFIER,
+   "Type" INFORMATION_SCHEMA.CHARACTER_DATA
 );
 
 --Prototype of a row that will be returned by the public.describe function
 --Matches the output of the contained SELECT query
 CREATE TYPE public.describe_retrun AS (
-	"Table Name" TEXT,
-	"Column Name" TEXT,
-	"Data Type" TEXT,
-	"Maximum Length" INTEGER
+   "Table Name" INFORMATION_SCHEMA.SQL_IDENTIFIER,
+   "Column Name" INFORMATION_SCHEMA.SQL_IDENTIFIER,
+   "Data Type" INFORMATION_SCHEMA.CHARACTER_DATA,
+   "Maximum Length" INFORMATION_SCHEMA.CARDINAL_NUMBER
 );
 
 --Returns a list of tables and views in the current user's schema
@@ -35,9 +35,9 @@ CREATE TYPE public.describe_retrun AS (
 CREATE OR REPLACE FUNCTION public.list_tables()
 RETURNS SETOF public.list_tables_return
 AS $$
-	SELECT table_name, table_type
-	FROM INFORMATION_SCHEMA.TABLES
-	WHERE table_schema = current_user;
+   SELECT table_name, table_type
+   FROM INFORMATION_SCHEMA.TABLES
+   WHERE table_schema = current_user;
 $$
 LANGUAGE SQL;
 
@@ -45,28 +45,31 @@ LANGUAGE SQL;
 CREATE OR REPLACE FUNCTION public.list_tables(TEXT)
 RETURNS SETOF public.list_tables_return
 AS $$
-	SELECT table_name, table_type
-	FROM INFORMATION_SCHEMA.TABLES
-	WHERE table_schema = $1;
+   SELECT table_name, table_type
+   FROM INFORMATION_SCHEMA.TABLES
+   WHERE table_schema = $1;
 $$
 LANGUAGE SQL;
 
+--Returns a list of columns in the specified table or view
+--Will only work on tables in the user's search_path
 CREATE OR REPLACE FUNCTION public.describe(TEXT)
 RETURNS SETOF public.describe_retrun
 AS $$
-	SELECT table_name, column_name, data_type, character_maximum_length 
-	FROM INFORMATION_SCHEMA.COLUMNS 
-	WHERE table_name = $1
-	AND table_schema =  current_user;
+   SELECT table_name, column_name, data_type, character_maximum_length 
+   FROM INFORMATION_SCHEMA.COLUMNS 
+   WHERE table_name = $1
+   AND table_schema =  current_user;
 $$
 LANGUAGE SQL;
 
+--Returns a list of columns in the specified table or view in the specified schema
 CREATE OR REPLACE FUNCTION public.describe(TEXT, TEXT)
 RETURNS SETOF public.describe_retrun
 AS $$
-	SELECT table_name, column_name, data_type, character_maximum_length 
-	FROM INFORMATION_SCHEMA.COLUMNS 
-	WHERE table_name = $2
-	AND table_schema = $1;
+   SELECT table_name, column_name, data_type, character_maximum_length 
+   FROM INFORMATION_SCHEMA.COLUMNS 
+   WHERE table_name = $2
+   AND table_schema = $1;
 $$
 LANGUAGE SQL;
