62175b5ecb5bdaf1d3215ef77c35d8a9c3b29f81
Andrew Figueroa <figueroa039@connect.wcsu.edu>
2017-06-14 21:06:50 -0400

    Add connection limit to student roles in createStudent()
    An ALTER ROLE statement was added that sets the connection limit for students
    created by createStudent to 5.

--- pgInstance/prepareClassDB.sql
+++ pgInstance/prepareClassDB.sql
@@ -102,6 +102,7 @@ BEGIN
    END IF;
    EXECUTE format('GRANT Student TO %I', $1);
    EXECUTE format('GRANT USAGE ON SCHEMA %I TO Instructor', $1);
+   EXECUTE format('ALTER ROLE %I CONNECTION LIMIT 5', $1);
    INSERT INTO classdb.Student VALUES($1, $2, $3);
 END;
 $$ LANGUAGE plpgsql
