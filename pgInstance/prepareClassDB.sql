9349c30893b631e68a55cf142f943a2c9e5230dc
Andrew Figueroa <figueroa039@connect.wcsu.edu>
2017-06-15 21:16:13 -0400

    Update statement_timeout for students to 2 seconds

--- pgInstance/prepareClassDB.sql
+++ pgInstance/prepareClassDB.sql
@@ -99,7 +99,7 @@ BEGIN
    EXECUTE format('GRANT Student TO %I', $1);
    EXECUTE format('GRANT USAGE ON SCHEMA %I TO Instructor', $1);
    EXECUTE format('ALTER ROLE %I CONNECTION LIMIT 5', $1);
-   EXECUTE format('ALTER ROLE %I SET statement_timeout = 15000', $1);
+   EXECUTE format('ALTER ROLE %I SET statement_timeout = 2000', $1);
    INSERT INTO classdb.Student VALUES($1, $2, $3) ON CONFLICT DO NOTHING;
 END;
 $$ LANGUAGE plpgsql
