cb0a8e789d115bddfb0c6c58088c0437f94c17a1
Andrew Figueroa <figueroa039@connect.wcsu.edu>
2017-06-19 20:59:20 -0400

    Move both password procedures into prepareClassDB
    Before, password procedures were located in a separate file
    (passwordProcedures.sql). However, these have now been moved to
    prepareClassDB.sql to improve ease of use.

--- pgInstance/prepareClassDB.sql
+++ pgInstance/prepareClassDB.sql
@@ -9,12 +9,13 @@
 --This script should be run as a user with superuser privileges, due to the need to create an
 -- event trigger.
 
-
 --This script first prevents student roles from modiying the public schema, and then creates a
--- classdb schema. Following that, a stored procedure for creating any type of user is defined.
--- Finally, procedures for creating and dropping students and instructors are defined. This script
--- also creates Student and Instructor tables in the classdb schema, and an event trigger that
--- records the timestamp of the last ddl statement issued by each student.
+-- classdb schema. Following that, a stored procedure for creating any type of user is defined,
+-- along with procedures for creating and dropping students and instructors. Finally,
+-- procedures for resetting a users password are created. This script also creates Student and
+-- Instructor tables in the classdb schema, and an event trigger that records the timestamp of
+-- the last ddl statement issued by each student.
+
 
 START TRANSACTION;
 
@@ -292,6 +293,73 @@ $$ LANGUAGE plpgsql
 REVOKE ALL ON FUNCTION classdb.dropUser(userName VARCHAR(50)) FROM PUBLIC;
 ALTER FUNCTION classdb.dropUser(userName VARCHAR(50)) OWNER TO DBManager;
 GRANT EXECUTE ON FUNCTION classdb.dropUser(userName VARCHAR(50)) TO Instructor;
+
+--The following procedure allows changing the password for a given username, given both the
+-- username and password. NOTICEs are raised if the user does not exist or if the password
+-- does not meet the requirements.
+--Current password requirements:
+-- - Must be 4 or more characters
+-- - Must contain at least one numerical digit (0-9)
+
+CREATE OR REPLACE FUNCTION classdb.changeUserPassword(userName VARCHAR(50), password VARCHAR(128)) RETURNS VOID AS
+$$
+DECLARE
+   userExists BOOLEAN;
+BEGIN
+   SELECT 1 FROM pg_catalog.pg_roles WHERE rolname = $1 INTO userExists;
+   IF userExists THEN
+      IF
+         LENGTH(password) > 3 AND
+         SUBSTRING(password from '[0-9]') IS NOT NULL
+      THEN
+         EXECUTE format('ALTER ROLE %I ENCRYPTED PASSWORD %L', userName, password);
+      ELSE
+         RAISE NOTICE 'Password does not meet requirements. Must be 6 or more characters and contain at least 1 number';
+      END IF;
+   ELSE
+      RAISE NOTICE 'User: "%" does not exist', userName;
+   END IF;
+END;
+$$ LANGUAGE plpgsql
+   SECURITY DEFINER;
+
+REVOKE ALL ON FUNCTION classdb.changeUserPassword(userName VARCHAR(50), password VARCHAR(128)) FROM PUBLIC;
+ALTER FUNCTION classdb.changeUserPassword(userName VARCHAR(50), password VARCHAR(128)) OWNER TO DBManager;
+
+
+--The following procedure resets a users password to the default password given a username.
+-- NOTE: The default password is not the same as the initialpassword that may have been given
+-- at the time of role creation. It is either the ID or username for a student and the username
+-- for an instructor.
+
+CREATE OR REPLACE FUNCTION classdb.resetUserPassword(userName VARCHAR(50)) RETURNS VOID AS
+$$
+DECLARE
+   studentID VARCHAR(128);
+BEGIN
+   IF
+      pg_catalog.pg_has_role($1, 'student', 'member')
+   THEN
+      SELECT ID FROM classdb.Student WHERE userName = $1 INTO studentID;
+      IF studentID IS NULL THEN
+         PERFORM classdb.changeUserPassword(userName, userName);
+      ELSE
+         PERFORM classdb.changeUserPassword(userName, studentID);
+      END IF;
+   ELSIF
+      pg_catalog.pg_has_role(userName, 'instructor', 'member')
+   THEN
+      PERFORM classdb.changeUserPassword(userName, userName);
+   ELSE
+      RAISE NOTICE 'User "%" not found among registered users', userName;
+   END IF;
+END;
+$$ LANGUAGE plpgsql
+   SECURITY DEFINER;
+
+REVOKE ALL ON FUNCTION classdb.resetUserPassword(userName VARCHAR(50)) FROM PUBLIC;
+ALTER FUNCTION classdb.resetUserPassword(userName VARCHAR(50)) OWNER TO DBManager;
+GRANT EXECUTE ON FUNCTION classdb.resetUserPassword(userName VARCHAR(50)) TO Instructor;
 
 
 CREATE TABLE IF NOT EXISTS classdb.Student
