d3d5fcfb5e0f8157759bfa6d1c40b9bf3ce9c604
Steven Rollo <srrollo@gmail.com>
2017-06-22 19:23:51 -0400

    Removed variable from superuser test. Fixes #19

--- pgInstance/prepareUserLogging.sql
+++ pgInstance/prepareUserLogging.sql
@@ -18,11 +18,8 @@ START TRANSACTION;
 --Check for superuser
 DO
 $$
-DECLARE
-   isSuper BOOLEAN;
 BEGIN
-   EXECUTE 'SELECT COALESCE(rolsuper, FALSE) FROM pg_catalog.pg_roles WHERE rolname = current_user' INTO isSuper;
-   IF NOT isSuper THEN
+   IF NOT EXISTS(SELECT * FROM pg_catalog.pg_roles WHERE rolname = current_user AND rolsuper = t) THEN
       RAISE EXCEPTION 'Insufficient privileges for script: must be run as a superuser';
    END IF;
 END
