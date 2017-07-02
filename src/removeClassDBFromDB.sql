658846771424db6642df20f2f003804c3a0b3660
Sean Murthy <murthys@wcsu.edu>
2017-07-01 21:38:17 -0400

    Created initial uninstall scripts
    Response to issue #76

---
+++ src/removeClassDBFromDB.sql
@@ -0,0 +1,63 @@
+--removeClassDBFromDB.sql - ClassDB
+
+--Sean Murthy
+--Data Science & Systems Lab (DASSL), Western Connecticut State University (WCSU)
+
+--(C) 2017- DASSL. ALL RIGHTS RESERVED.
+--Licensed to others under CC 4.0 BY-SA-NC
+--https://creativecommons.org/licenses/by-nc-sa/4.0/
+
+--PROVIDED AS IS. NO WARRANTIES EXPRESSED OR IMPLIED. USE AT YOUR OWN RISK.
+
+
+--This script undoes the changes ClassDB installation scripts made to current DB
+-- not all changes can be undone
+-- this script lists the changes an appropriate user has to perform separately
+
+--This script will NOT drop user roles
+-- BEFORE running this script: run appropriate classDB.dropXYZ functions in each
+-- database where ClassDB is installed
+
+--This script should be run by a superuser
+
+
+START TRANSACTION;
+
+--Make sure the current user has sufficient privilege to run this script
+-- privileges required: superuser
+DO
+$$
+BEGIN
+   IF NOT EXISTS (SELECT * FROM pg_catalog.pg_roles
+                  WHERE rolname = current_user AND rolsuper = TRUE
+                 ) THEN
+      RAISE EXCEPTION 'Insufficient privileges: script must be run as a user with'
+                        ' superuser privileges';
+   END IF;
+END
+$$;
+
+--event triggers
+DROP EVENT TRIGGER IF EXISTS updateStudentActivityTriggerDDL;
+DROP EVENT TRIGGER IF EXISTS updateStudentActivityTriggerDrop;
+
+--remove membership of students, instructors, and db managers
+-- TBD
+
+--Delete the entire classdb schema in the current database
+-- no need to drop individual objects created in that schema
+DROP SCHEMA IF EXISTS ClassDB CASCADE;
+
+--change ownership of the current DB to current user so they can delete the DB
+-- TBD
+
+--create a list of things users have to do on their own
+-- commenting out the RAISE NOTICE statements because they cause syntax error
+
+--RAISE NOTICE 'Drop user roles or adjust their privileges';
+--RAISE NOTICE 'Adjust privileges on PUBLIC schema if appropriate';
+--RAISE NOTICE 'Run DROP DATABASE statement to remove the database if appropriate';
+--RAISE NOTICE 'Run prepareClassServer.sql after removing ClassDB '
+--             'from other databases';
+
+COMMIT;
---
+++ src/removeClassDBFromServer.sql
@@ -0,0 +1,54 @@
+--removeClassDBFromServer.sql - ClassDB
+
+--Sean Murthy
+--Data Science & Systems Lab (DASSL), Western Connecticut State University (WCSU)
+
+--(C) 2017- DASSL. ALL RIGHTS RESERVED.
+--Licensed to others under CC 4.0 BY-SA-NC
+--https://creativecommons.org/licenses/by-nc-sa/4.0/
+
+--PROVIDED AS IS. NO WARRANTIES EXPRESSED OR IMPLIED. USE AT YOUR OWN RISK.
+
+
+--This script undoes the changes the ClassDB installation scripts make to the server
+-- not all changes can be undone
+-- this script lists the changes an appropriate user has to perform separately
+
+--This script must be run AFTER running removeClassDBFromDB.sql on all databases
+-- where ClassDB is installed
+
+--This script will NOT drop user roles
+-- BEFORE running this script: run appropriate classDB.dropXYZ functions in each
+-- database where ClassDB is installed
+
+--This script should be run by a superuser
+
+
+START TRANSACTION;
+
+--Make sure the current user has sufficient privilege to run this script
+-- privileges required: superuser
+DO
+$$
+BEGIN
+   IF NOT EXISTS (SELECT * FROM pg_catalog.pg_roles
+                  WHERE rolname = current_user AND rolsuper = TRUE
+                 ) THEN
+      RAISE EXCEPTION 'Insufficient privileges: script must be run as a user with'
+                        ' superuser privileges';
+   END IF;
+END
+$$;
+
+--Drop app-specific roles
+DROP ROLE IF EXISTS Instructor;
+DROP ROLE IF EXISTS DBManager;
+DROP ROLE IF EXISTS Student;
+DROP ROLE IF EXISTS ClassDB;
+
+--create a list of things users have to do on their own
+-- commenting out the RAISE NOTICE statement because it causes syntax error
+
+--RAISE NOTICE 'Run ALTER SYSTEM statements to disable/modify logging';
+
+COMMIT;
