75e4e490b828ed5e404af0025984867e36762089
Steven Rollo <srrollo@gmail.com>
2017-07-06 20:05:37 -0400

    Removed reference to cs205 in shelter files

--- examples/populateShelterSchema.sql
+++ examples/populateShelterSchema.sql
@@ -1,4 +1,4 @@
---populateShelterSchema.sql - Schemas for CS205
+--populateShelterSchema.sql - Example schemas for ClassDB
 
 --Andrew Figueroa, Steven Rollo, Sean Murthy
 --Data Science & Systems Lab (DASSL), Western Connecticut State University (WCSU)
