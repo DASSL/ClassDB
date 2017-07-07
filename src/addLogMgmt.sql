80529542ea3e8f6168997e0fca1aef3112c8ce20
Steven Rollo <srrollo@gmail.com>
2017-07-07 13:35:49 -0400

    Added addLogMgmt.sql

---
+++ src/addLogMgmt.sql
@@ -0,0 +1,10 @@
+--addLogMgmt.sql - ClassDB
+
+--Andrew Figueroa, Steven Rollo, Sean Murthy
+--Data Science & Systems Lab (DASSL), Western Connecticut State University (WCSU)
+
+--(C) 2017- DASSL. ALL RIGHTS RESERVED.
+--Licensed to others under CC 4.0 BY-SA-NC
+--https://creativecommons.org/licenses/by-nc-sa/4.0/
+
+--PROVIDED AS IS. NO WARRANTIES EXPRESSED OR IMPLIED. USE AT YOUR OWN RISK.
