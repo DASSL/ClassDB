905bd179024f7119a5892d62e929127f6b8ea952
Andrew Figueroa <figueroa039@connect.wcsu.edu>
2017-07-06 20:47:00 -0400

    Make foldPgID match other helper functions
    Place into classdb schema, assign ClassDB as owner, line break function
    signature.

--- src/addHelperFunctions.sql
+++ src/addHelperFunctions.sql
@@ -170,15 +170,21 @@ ALTER FUNCTION
 --Define a function to replicate PostgreSQL's folding behavior for SQL IDs
 -- If identifier is quoted, then the same value is retuned with quotes removed
 -- If it is not, then identifier is returned, but made lowercase
-CREATE OR REPLACE FUNCTION foldPgID(identifier VARCHAR(65)) RETURNS VARCHAR(63) AS
+CREATE OR REPLACE FUNCTION
+   classdb.foldPgID(identifier VARCHAR(65))
+   RETURNS VARCHAR(63) AS
 $$
 SELECT CASE WHEN SUBSTRING($1 from 1 for 1) = '"' AND
                  SUBSTRING($1 from LENGTH($1) for 1) = '"' THEN
                    SUBSTRING($1 from 2 for LENGTH($1) - 2)
-            ELSE 
+            ELSE
               LOWER($1)
        END;
 $$ LANGUAGE sql;
+
+ALTER FUNCTION
+   classb.foldPgID(identifier VARCHAR(65))
+   OWNER TO ClassDB;
 
 
 --Define a function to retrieve specific capabilities a user has
