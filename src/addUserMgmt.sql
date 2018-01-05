79ebb9a1ab2360ab52f869a86756e2abf6c26f24
Sean Murthy <murthys@wcsu.edu>
2018-01-04 20:35:57 -0500

    Replaced "isRoleDefined" w "isServerRoleDefined"
    Replaced calls to function "isRoleDefined" with "isServerRoleDefined" because the function is renamed in addHelpers.sql

--- src/addUserMgmt.sql
+++ src/addUserMgmt.sql
@@ -45,7 +45,7 @@ CREATE FUNCTION
    classdb.createUser(userName VARCHAR(63), initialPwd VARCHAR(128)) RETURNS VOID AS
 $$
 BEGIN
-   IF classdb.isRoleDefined($1) THEN
+   IF classdb.isServerRoleDefined($1) THEN
       RAISE NOTICE 'User "%" already exists, password not modified', $1;
    ELSE
       EXECUTE
@@ -251,7 +251,7 @@ DROP FUNCTION IF EXISTS classdb.dropStudent(userName VARCHAR(63));
 CREATE FUNCTION classdb.dropStudent(userName VARCHAR(63)) RETURNS VOID AS
 $$
 BEGIN
-   IF classdb.isRoleDefined($1) AND
+   IF classdb.isServerRoleDefined($1) AND
       pg_catalog.pg_has_role(classdb.foldPgID($1), 'classdb_student', 'member')
    THEN
       EXECUTE format('REVOKE ClassDB_Student FROM %s', $1);
@@ -307,7 +307,7 @@ DROP FUNCTION IF EXISTS classdb.dropInstructor(userName VARCHAR(63));
 CREATE FUNCTION classdb.dropInstructor(userName VARCHAR(63)) RETURNS VOID AS
 $$
 BEGIN
-   IF classdb.isRoleDefined($1) AND
+   IF classdb.isServerRoleDefined($1) AND
       pg_catalog.pg_has_role(classdb.foldPgID($1), 'classdb_instructor', 'member')
    THEN
       EXECUTE format('REVOKE ClassDB_Instructor FROM %s', $1);
@@ -356,7 +356,7 @@ DROP FUNCTION IF EXISTS classdb.dropDBManager(userName VARCHAR(63));
 CREATE FUNCTION classdb.dropDBManager(userName VARCHAR(63)) RETURNS VOID AS
 $$
 BEGIN
-   IF classdb.isRoleDefined($1) AND
+   IF classdb.isServerRoleDefined($1) AND
       pg_catalog.pg_has_role(classdb.foldPgID($1), 'classdb_dbmanager', 'member')
    THEN
       EXECUTE format('REVOKE ClassDB_DBManager FROM %s', userName);
@@ -402,7 +402,7 @@ CREATE FUNCTION classdb.resetUserPassword(userName VARCHAR(63))
    RETURNS VOID AS
 $$
 BEGIN
-   IF classdb.isRoleDefined($1) THEN
+   IF classdb.isServerRoleDefined($1) THEN
       EXECUTE format('ALTER ROLE %s ENCRYPTED PASSWORD %L', userName, userName);
    ELSE
       RAISE NOTICE 'User "%" not found among registered users', userName;
