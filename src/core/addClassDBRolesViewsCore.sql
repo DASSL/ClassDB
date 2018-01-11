828c6bba4cc81bdb43b99b4294bd3c412edc8d61
DESKTOP-CCFFGEO\Kevin <kelly263@connect.wcsu.edu>
2018-01-11 16:36:54 -0500

    Add-Views-core
    Moved views User, Student, Instructor, and DBManager to own file in src/core

---
+++ src/core/addClassDBRolesViewsCore.sql
@@ -0,0 +1,70 @@
+--addClassDBRolesViewsCore.sql - ClassDB
+
+--Andrew Figueroa, Kevin Kelly, Sean Murthy
+--Data Science & Systems Lab (DASSL)
+--https://dassl.github.io/
+
+--(C) 2017- DASSL. ALL RIGHTS RESERVED.
+--Licensed to others under CC 4.0 BY-SA-NC
+--https://creativecommons.org/licenses/by-nc-sa/4.0/
+
+--PROVIDED AS IS. NO WARRANTIES EXPRESSED OR IMPLIED. USE AT YOUR OWN RISK.
+
+--Define a view to return known users
+-- the fields marked TBD are yet to be filled in: they are commented out so
+-- the view definition can be replaced later
+CREATE OR REPLACE VIEW ClassDB.User AS
+SELECT
+   RoleName AS UserName, FullName, SchemaName, ExtraInfo,
+   ClassDB.IsInstructor(RoleName) AS IsInstructor,
+   ClassDB.IsStudent(RoleName) AS IsStudent,
+   ClassDB.IsDBManager(RoleName) AS IsDBManager,
+   ClassDB.hasClassDBRole(RoleName) AS HasClassDBRole
+   --0 AS DDLCount,              --TBD
+   --NULL AS LastDDLObject,      --TBD
+   --NULL AS LastDDLActivityAt,  --TBD
+   --0 AS ConnectionCount,       --TBD
+   --NULL AS LastConnectionAtUTC --TBD
+FROM ClassDB.RoleBase
+WHERE NOT IsTeam;
+
+ALTER VIEW ClassDB.User OWNER TO ClassDB;
+REVOKE ALL PRIVILEGES ON ClassDB.User FROM PUBLIC;
+GRANT SELECT ON ClassDB.User TO ClassDB_Instructor, ClassDB_DBManager;
+
+--Define views to obtain info on known instructors, students, and DB managers
+-- these views obtain information from the previously defined ClassDB.User view
+CREATE OR REPLACE VIEW ClassDB.Instructor AS
+   SELECT UserName, FullName, SchemaName, ExtraInfo, IsStudent, IsDBManager
+--TBD:    DDLCount, LastDDLOperation, LastDDLObject, LastDDLActivityAtUTC,
+--TBD:    ConnectionCount, LastConnectionAtUTC
+   FROM ClassDB.User
+   WHERE IsInstructor;
+
+ALTER VIEW ClassDB.Instructor OWNER TO ClassDB;
+REVOKE ALL PRIVILEGES ON ClassDB.Instructor FROM PUBLIC;
+GRANT SELECT ON ClassDB.Instructor TO ClassDB_Instructor, ClassDB_DBManager;
+
+
+CREATE OR REPLACE VIEW ClassDB.Student AS
+   SELECT UserName, FullName, SchemaName, ExtraInfo, IsInstructor, IsDBManager
+--TBD:    DDLCount, LastDDLOperation, LastDDLObject, LastDDLActivityAtUTC,
+--TBD:    ConnectionCount, LastConnectionAtUTC
+   FROM ClassDB.User
+   WHERE IsStudent;
+
+ALTER VIEW ClassDB.Student OWNER TO ClassDB;
+REVOKE ALL PRIVILEGES ON ClassDB.Student FROM PUBLIC;
+GRANT SELECT ON ClassDB.Student TO ClassDB_Instructor, ClassDB_DBManager;
+
+
+CREATE OR REPLACE VIEW ClassDB.DBManager AS
+   SELECT UserName, FullName, SchemaName, ExtraInfo, IsInstructor, IsStudent
+--TBD:    DDLCount, LastDDLOperation, LastDDLObject, LastDDLActivityAtUTC,
+--TBD:    ConnectionCount, LastConnectionAtUTC
+   FROM ClassDB.User
+   WHERE IsDBManager;
+
+ALTER VIEW ClassDB.DBManager OWNER TO ClassDB;
+REVOKE ALL PRIVILEGES ON ClassDB.DBManager FROM PUBLIC;
+GRANT SELECT ON ClassDB.DBManager TO ClassDB_Instructor, ClassDB_DBManager;
