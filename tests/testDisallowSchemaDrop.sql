ec8eab80ddc1c13331441c23cddd6bd7a142df37
Sean Murthy <murthys@wcsu.edu>
2018-06-09 20:38:06 -0400

    Expand tests
    Added new tests and streamlined existing tests.
    
    A test case fails: new schema creation by student user (fail code 3). This failure is expected because a Teams discussion establishes users are presently unable to create own schemas. That issue is being discussed.

--- tests/testDisallowSchemaDrop.sql
+++ tests/testDisallowSchemaDrop.sql
@@ -14,8 +14,6 @@
 --This script should be run as a superuser
 
 --This script tests the functionality in addDisallowSchemaDropReco.sql
--- only nominal tests are covered presently
--- need to plan a test for cases that should cause exceptions
 
 
 START TRANSACTION;
@@ -35,110 +33,215 @@ DO
 $$
 BEGIN
 
-   --student-initiated schema drop should be disallowed by default
+   --test if student-initiated schema drop is disallowed by default
    RAISE INFO '%   isSchemaDropAllowed',
    CASE ClassDB.isSchemaDropAllowed()
       WHEN TRUE THEN 'FAIL: Code 1' ELSE 'PASS'
    END;
 
-   --test with a student user: should not be able to drop any schema
-   BEGIN
-      PERFORM ClassDB.createStudent('s1', 's1 name');
-      SET SESSION AUTHORIZATION s1;
-      DROP SCHEMA s1; --should cause exception
-      RAISE INFO '%   drop schema by student disallowed', 'FAIL: Code 2';
+--------------------------------------------------------------------------------
 
-      EXCEPTION
-         WHEN OTHERS THEN
-            RAISE INFO '%   drop schema by student disallowed', 'PASS';
+   --test with a student: should be able to drop tables but not a schema
+
+   PERFORM ClassDB.createStudent('s1', 's1 name');
+   SET SESSION AUTHORIZATION s1;
+
+   --table create and drop: should not cause exception
+   BEGIN
+      CREATE TABLE t1(a integer);
+      DROP TABLE t1;
+      RAISE INFO '%   drop table by student', 'PASS';
+
+   EXCEPTION
+      WHEN OTHERS THEN
+         RAISE INFO '%   drop table by student', 'FAIL: Code 2'
+                    USING HINT = SQLERRM;
    END;
 
-   --revert to original user
-   RESET SESSION AUTHORIZATION;
-
-   --test with an instructor: should be able to drop any schema
+   --schema create: should not cause exception
    BEGIN
-      PERFORM ClassDB.createInstructor('i1', 'i1 name');
-      SET SESSION AUTHORIZATION i1;
-      DROP SCHEMA i1; --should not cause exception
-      RAISE INFO '%   drop schema by instructor', 'PASS';
+      CREATE SCHEMA schema_created_by_s1;
+      RAISE INFO '%   create schema by student', 'PASS';
 
-      EXCEPTION
-         WHEN OTHERS THEN
-            RAISE INFO '%   drop schema by instructor', 'FAIL: Code 3'
-                       USING HINT = SQLERRM;
+   EXCEPTION
+      WHEN OTHERS THEN
+         RAISE INFO '%   create schema by student', 'FAIL: Code 3'
+                    USING HINT = SQLERRM;
    END;
 
-   --revert to original user
-   RESET SESSION AUTHORIZATION;
-
-   --test with a DB manager: should be able to drop any schema
+   --schema drop (schema created by student): should cause exception
    BEGIN
-      PERFORM ClassDB.createDBManager('d1', 'd1 name');
-      SET SESSION AUTHORIZATION d1;
-      DROP SCHEMA d1; --should not cause exception
-      RAISE INFO '%   drop schema by DB manager', 'PASS';
+      DROP scHema schema_created_by_s1; --spelling 'scHema' intentional
+      RAISE INFO '%   drop schema created by student disallowed', 'FAIL: Code 4';
 
-      EXCEPTION
-         WHEN OTHERS THEN
-            RAISE INFO '%   drop schema by DB manager', 'FAIL: Code 4'
-                       USING HINT = SQLERRM;
+   EXCEPTION
+      WHEN OTHERS THEN
+         RAISE INFO '%   drop schema created by student disallowed', 'PASS';
    END;
 
-   --revert to original user
-   RESET SESSION AUTHORIZATION;
-
-   --test with a non-ClassDB user: should be able to drop any schema
+   --schema drop (schema assigned to student): should cause exception
    BEGIN
-      CREATE USER u1;
-      CREATE SCHEMA schema_u1 AUTHORIZATION u1;
+      DROP SCHEMA s1;
+      RAISE INFO '%   drop schema assigned to student disallowed', 'FAIL: Code 5';
 
-      SET SESSION AUTHORIZATION u1;
-      DROP SCHEMA schema_u1; --should not cause exception
-      RAISE INFO '%   drop schema by non-ClassDB user', 'PASS';
-
-      EXCEPTION
-         WHEN OTHERS THEN
-            RAISE INFO '%   drop schema by non-ClassDB user', 'FAIL: Code 5'
-                       USING HINT = SQLERRM;
+   EXCEPTION
+      WHEN OTHERS THEN
+         RAISE INFO '%   drop schema assigned to student disallowed', 'PASS';
    END;
 
+   --drop owned objects: should cause exception
+   BEGIN
+      DrOP oWNeD BY CURRENT_USER; --spelling 'DrOP oWNeD' intentional
+      RAISE INFO '%   drop owned by student disallowed', 'FAIL: Code 6';
+
+   EXCEPTION
+      WHEN OTHERS THEN
+         RAISE INFO '%   drop owned by student disallowed', 'PASS';
+   END;
 
 --------------------------------------------------------------------------------
 
-   --revert to original user
-   RESET SESSION AUTHORIZATION;
+   --test with an instructor: should be able to drop any object
 
-   --allow student-initiated schema drop
+   RESET SESSION AUTHORIZATION;
+   PERFORM ClassDB.createInstructor('i1', 'i1 name');
+   SET SESSION AUTHORIZATION i1;
+
+   --schema drop: should not cause exception
+   BEGIN
+      DROP SCHEMA i1;
+      RAISE INFO '%   drop schema by instructor', 'PASS';
+
+   EXCEPTION
+      WHEN OTHERS THEN
+         RAISE INFO '%   drop schema by instructor', 'FAIL: Code 7'
+                    USING HINT = SQLERRM;
+   END;
+
+   --drop owned objects: should not cause exception
+   BEGIN
+      DROP OWNED BY CURRENT_USER;
+      RAISE INFO '%   drop owned by instructor', 'PASS';
+
+   EXCEPTION
+      WHEN OTHERS THEN
+         RAISE INFO '%   drop owned by instructor', 'FAIL: Code 8'
+                    USING HINT = SQLERRM;
+   END;
+
+--------------------------------------------------------------------------------
+
+   --test with a DB manager: should be able to drop any object
+
+   RESET SESSION AUTHORIZATION;
+   PERFORM ClassDB.createDBManager('d1', 'd1 name');
+   SET SESSION AUTHORIZATION d1;
+
+   --schema drop: should not cause exception
+   BEGIN
+      DROP SCHEMA d1;
+      RAISE INFO '%   drop schema by DB manager', 'PASS';
+
+   EXCEPTION
+      WHEN OTHERS THEN
+         RAISE INFO '%   drop schema by DB manager', 'FAIL: Code 9'
+                    USING HINT = SQLERRM;
+   END;
+
+   --drop owned objects: should not cause exception
+   BEGIN
+      DROP OWNED BY CURRENT_USER;
+      RAISE INFO '%   drop owned by DB manager', 'PASS';
+
+   EXCEPTION
+      WHEN OTHERS THEN
+         RAISE INFO '%   drop owned by DB manager', 'FAIL: Code 10'
+                    USING HINT = SQLERRM;
+   END;
+
+--------------------------------------------------------------------------------
+
+   --test with a non-ClassDB user: should be able to drop any schema/object
+
+   RESET SESSION AUTHORIZATION;
+   CREATE USER u1;
+   CREATE SCHEMA schema_u1 AUTHORIZATION u1;
+   SET SESSION AUTHORIZATION u1;
+
+   --schema drop: should not cause exception
+   BEGIN
+      DROP SCHEMA schema_u1;
+      RAISE INFO '%   drop schema by non-ClassDB user', 'PASS';
+
+   EXCEPTION
+      WHEN OTHERS THEN
+         RAISE INFO '%   drop schema by non-ClassDB user', 'FAIL: Code 10'
+                    USING HINT = SQLERRM;
+   END;
+
+   --drop owned objects: should not cause exception
+   BEGIN
+      DROP OWNED BY CURRENT_USER;
+      RAISE INFO '%   drop owned by non-ClassDB user', 'PASS';
+
+   EXCEPTION
+      WHEN OTHERS THEN
+         RAISE INFO '%   drop owned by non-ClassDB user', 'FAIL: Code 11'
+                    USING HINT = SQLERRM;
+   END;
+
+--------------------------------------------------------------------------------
+
+   --test allowing student-initiated schema drop
+
+   --allow student-initiated schema drop as an instructor
+   RESET SESSION AUTHORIZATION;
+   SET SESSION AUTHORIZATION i1;
+
    PERFORM ClassDB.allowSchemaDrop();
    RAISE INFO '%   allowSchemaDrop',
       CASE ClassDB.isSchemaDropAllowed()
-         WHEN TRUE THEN 'PASS' ELSE 'FAIL: Code 6'
+         WHEN TRUE THEN 'PASS' ELSE 'FAIL: Code 12'
       END;
 
-   --student should be able to drop schema
+   --test schema drop with a student: should be able to drop any object
+   RESET SESSION AUTHORIZATION;
+   SET SESSION AUTHORIZATION s1;
+
+   --schema drop: should not cause exception
    BEGIN
-      PERFORM ClassDB.createStudent('s2', 's2 name');
-      SET SESSION AUTHORIZATION s2;
-      DROP SCHEMA s2; --should not cause exception
+      DROP SCHEMA s1;
       RAISE INFO '%   drop schema by student allowed', 'PASS';
 
-      EXCEPTION
-         WHEN OTHERS THEN
-            RAISE INFO '%   drop schema by student allowed', 'FAIL: Code 7'
-                       USING HINT = SQLERRM;
+   EXCEPTION
+      WHEN OTHERS THEN
+         RAISE INFO '%   drop schema by student allowed', 'FAIL: Code 13'
+                    USING HINT = SQLERRM;
+   END;
+
+   --drop owned objects: should not cause exception
+   BEGIN
+      DROP OWNED BY CURRENT_USER;
+      RAISE INFO '%   drop owned by student allowed', 'PASS';
+
+   EXCEPTION
+      WHEN OTHERS THEN
+         RAISE INFO '%   drop owned by student allowed', 'FAIL: Code 14'
+                    USING HINT = SQLERRM;
    END;
 
 --------------------------------------------------------------------------------
 
-   --revert to original user
-   RESET SESSION AUTHORIZATION;
+   --test changing back to disallowing student-initiated schema drop
 
-   --disallow student-initiated schema drop
+   --disallow as an instructor
+   RESET SESSION AUTHORIZATION;
+   SET SESSION AUTHORIZATION i1;
+
    PERFORM ClassDB.disallowSchemaDrop();
    RAISE INFO '%   disallowSchemaDrop',
       CASE ClassDB.isSchemaDropAllowed()
-         WHEN TRUE THEN 'FAIL: Code 7' ELSE 'PASS'
+         WHEN TRUE THEN 'FAIL: Code 15' ELSE 'PASS'
       END;
 
 
